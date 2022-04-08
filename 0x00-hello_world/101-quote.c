245b6655c457b961d65955e55bf9264d8f708d52
cantong93 <cantongjunior@gmail.com>
2022-04-08 10:18:31 -0700

    101 qoutes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 
 /**
-   * main - Entry point
+    * main - Entry point
      * Return: Always 0 (Success)
-       */
+      */
 int main(void)
 {
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
