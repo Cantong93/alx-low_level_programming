f9b54a183588ff71ab86ce2b9281474a418bb9e0
cantong93 <cantongjunior@gmail.com>
2022-04-09 18:23:48 -0700

    print aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the letters of the alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  /Declarations/
+    char letter;
+  for (letter = 'a'; letter <= 'z'; letter++)
+    {
+      putchar(letter);
+    }
+  putchar('\n');
+  return (0);
+}
