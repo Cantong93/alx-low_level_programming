b2a71924d1e4b1e62548e20180d2cf86a0db6393
cantong93 <cantongjunior@gmail.com>
2022-04-09 18:36:55 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  * main - prints the letters of the alphabets
- *
+ *  
  * Return: 0
  */
 int main(void)
 {
-  /Declarations/
-    char letter;
-  for (letter = 'a'; letter <= 'z'; letter++)
-    {
-      putchar(letter);
-    }
-  putchar('\n');
-  return (0);
+/Declarations/
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
 }
+putchar('\n');
+return (0);
+}
+
+
