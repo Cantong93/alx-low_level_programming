e804701944e99b8cece8e7685586a8e3bdd466c5
cantong93 <cantongjunior@gmail.com>
2022-04-11 12:52:30 -0700

    priny numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
+
+
 /**
- * main - prints the digits of the alphabets
- *
- * Return: 0
- */
+* main - prints the digits of the alphabets
+*
+* Return: 0
+*/
 
 int main(void)
 {
-Declaration/
+/Declarations/
 int digit;
 
 for (digit = 0; digit < 10; digit++)
-{
-printf("%d", digit);
-}
-
-putchar('\n');
-
-return (0);
-}
-       
+	   {
+	   printf("%d", digit);
+	   }
+	   putchar('\n');
+	   return (0);
+	   }
