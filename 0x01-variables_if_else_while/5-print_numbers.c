4ca0e7d17ea73a97c9643ba3e79432f1e0736aa1
cantong93 <cantongjunior@gmail.com>
2022-04-11 07:04:25 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,20 @@
+
 #include <stdio.h>
 /**
 * main - prints the digits of the alphabets
 *
+* Description: Print numbers 0 to 9 on one line
 * Return: 0
 */
 int main(void)
 {
-/Declarations/
-int digit;
-for (digit = 0; digit < 10; digit++)
-{
-printf("%d", digit);
-}
+char i;
+
+for (i = '0'; i <= '9'; ++i)
+putchar(i)
 
 putchar('\n');
 
 return (0);
 }
+       
