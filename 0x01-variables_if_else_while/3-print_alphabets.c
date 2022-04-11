f54011125f836408c824ac7f74b58e8898a7b15e
cantong93 <cantongjunior@gmail.com>
2022-04-10 20:37:35 -0700

    play with letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
-
 /**
  * main - entry point
  *
  * Description: Print lower and uppercase alphabet on the same line
  * Return: 0
  */
-
 int main(void)
 {
-  int c;
+int c;
 
-  for (c = 0; c < 26; ++c)
-    putchar('a' + c);
-  for (c = 0; c < 26; ++c)
-    putchar('A' + c);
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
