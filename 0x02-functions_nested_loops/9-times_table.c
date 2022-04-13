459be36d64aa5b128d752b414b249b149c3fdbf9
cantong93 <cantongjunior@gmail.com>
2022-04-13 16:37:06 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,48 @@
 #include "main.h"
 /**
- * times_table - prints the times table from 0 - 9.
-(*
-* Return:0
-*/
+ * times_table - prints the 9 times table
+ *
+ * Return: Doesn't return anything, void
+ */
 void times_table(void)
 {
-int i, j, res;
-for (i = 0; i <= 9; i++)
-{
+int i, j, product, x;
 for (j = 0; j <= 9; j++)
 {
-res = (i * j);
-if (j != 0)
+for (i = 0; i <= 9; i++)
+{
+product = j * i;
+if (i != 0)
 {
 _putchar(',');
+for (x = len(product); x <= len(9 * 9); x++)
 _putchar(' ');
-if (res >= 10)
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
 }
-else if (res < 10 && j != 0)
+if (product > 9)
 {
-_putchar(' ');
-_putchar((res % 10) + '0');
+_putchar(product / 10 + '0');
 }
-else
-_putchar((res % 10) + '0');
+_putchar(product % 10 + '0');
 }
 _putchar('\n');
 }
+}
+
+/**
+ * len - counts the number of digits in a number
+ * @n: number to be used
+ *
+ * Return: returns the number of digits
+ */
+int len(int n)
+{
+int out = 0;
+if (n == 0)
+return (1);
+while (n != 0)
+{
+n /= 10;
+out++;
+}
+return (out);
 }
