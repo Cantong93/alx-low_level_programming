fd818fe144fb36a661b1a15592ae88d2bca901b8
cantong93 <cantongjunior@gmail.com>
2022-04-13 12:46:48 -0700

    print 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*prnt_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*Description: prints 10 times the alphabet, in lowercase
 *
 */
 void print_alphabet_x10(void)
