a02d6f97807448a80c5c2e33433d3215f270a7e9
cantong93 <cantongjunior@gmail.com>
2022-04-20 16:27:36 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
 /**
- * _atoi - changes a string to an int
- * @s: the string to be changed
- *
- * Return: the converted int
- */
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
 int _atoi(char *s)
 {
 int i = 1;
@@ -12,9 +12,9 @@ do {
 if (*s == '-')
 i *= -1;
 else if (*s >= '0'  && *s <= '9')
-n = (n  *10 ) + (*s - '0');
+n = (n * 10) + (*s - '0');
 else if (n > 0)
 break;
 } while (*s++);
-return (n  *i);
+return (n * i);
 }
