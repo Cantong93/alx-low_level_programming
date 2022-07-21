d29495e185ea65c80355a2464da12017ae985f14
cantong93 <cantongjunior@gmail.com>
2022-07-21 03:41:50 -0700

    listing even 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a dlistint_t
+ * @head: The head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *enode;
+
+	while (head)
+	{
+		enode = head;
+		head = head->next;
+		free(enode);
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
